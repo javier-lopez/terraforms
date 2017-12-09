4e9522809d3817133f755777410c3674df7601f6
Javier López <m@javier.io>
2017-12-09 13:09:46 -0600

    'hola mundo' => hostname -f

--- haproxy-www/www-1.tf
+++ haproxy-www/www-1.tf
@@ -20,7 +20,7 @@ provisioner "remote-exec" {
       "export PATH=$PATH:/usr/bin",
       "sudo apt-get update",
       "sudo apt-get -y install nginx",
-      "echo 'hola mundo' | sudo tee /var/www/html/index.html"
+      "echo \"<html><head><title>$(hostname -f)</title></head><body><h1>$(hostname -f)</h1></body></html>\" | sudo tee /var/www/html/index.html"
     ]
   }
 }
